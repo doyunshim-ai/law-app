b9b2fc40f108e0a389a676decaff00b1fe9027de
doyunshim-ai <doyunshim@gmail.com>
2026-02-22 13:10:39 +0900

    Create Message schema

---
+++ backend/models/Message.ts
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+const messageSchema = new mongoose.Schema({
+  senderId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  receiverId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  caseId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Case',
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  isRead: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+export default mongoose.model('Message', messageSchema);
